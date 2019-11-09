71a7b6dfe8ee8f628dcccafcd76b222363f309ec
mikasd <michael.ingley@gmail.com>
2019-11-09 15:38:47 -0800

    testing

--- server/add.js
+++ server/add.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./database.js');
-const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
+
 
 router.post('/', (req, res) => {
 	console.log(req.body)
--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const cors = require('cors');
-
 const searchRouter = require('./add.js');
-
 const app = express();
-
-app.use(cors());
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
