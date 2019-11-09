109a2da665322eaeb62c687e2a2a137cb578a862
mikasd <michael.ingley@gmail.com>
2019-11-08 22:24:58 -0800

    mysql boilerplate

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.static('dist'));
+
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM flagged';
+
+const connection = mysql.createConnection({
+    host: 'hostaddressherejosh',
+    user: 'username',
+    password: 'password?',
+    database: 'hackathonDBname'
+})
+
+// connection.connect(err => {
+//     if(err){
+//         console.log(err);
+//         return err;
+//     }
+// });
 
 app.get('/api', (req, res) => {
     res.send('backend is working')
--- src/js/App.js
+++ src/js/App.js
@@ -3,16 +3,25 @@ import { Login } from './components/login/Login'
 
 class App extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = { counter: 0 };
-        this.handleClick = this.handleClick.bind(this); 
-      }
-
-    render(){
-        return(
-        <p>app component</p>
-        )
+      super(props);
+      this.state = {message: 'Hello!'};
+      // This line is important!
+      this.handleClick = this.handleClick.bind(this);
     }
-}
+  
+    handleClick() {
+      alert(this.state.message);
+    }
+  
+    render() {
+      return (
+        <div>
+            <h1>this is the main app component</h1>
+            <Login />
+        </div>
+      );
+    }
+  }
 
 export default App;
+
