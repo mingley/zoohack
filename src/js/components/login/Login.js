4517218d06a2197a343ec1a9efa7fb3f173cd058
Jin Kim <jhkim8268@gmail.com>
2019-11-09 15:39:55 -0800

    Revert "built login front-end component"

--- src/js/components/login/Login.js
+++ src/js/components/login/Login.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
-class Login extends React.Component {
-
-	render() {
-		return (
-			<form className='center' onSubmit={this.props.handleSubmit}>
-				<div className='center'>
-					<input onChange={this.props.userInput} type='text' placeholder='id' id='username' onChange={this.props.updateID} />
-				</div>
-				<div className='center'>
-					<input type='password' placeholder='password' id='password' onChange={this.props.updatePW} />
-				</div>
-				<div className='center'>
-					<button type='submit' disabled={!this.props.validateForm()}>Login</button>
-				</div>
-			</form>
-		);
-	}
+export const Login = () => {
+    return(
+        <p>--login here??</p>
+    )
 }
-
-export default Login;
