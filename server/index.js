681b3f2d7de8215785ea90454bcdf791b4296b36
Joshua Brooks <josh@jbrooks.me>
2019-11-09 11:18:07 -0800

    Testing searches

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const app = require('./server');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on http://http://138.68.208.80/:${PORT}`));
