2fef8278b6c4420e6841eb4ac46edede4c4bbed4
mikasd <michael.ingley@gmail.com>
2019-11-09 15:39:17 -0800

    testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const searchRouter = require('./add.js');
 const app = express();
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
