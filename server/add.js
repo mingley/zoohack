f2264798566e6a74cc54aaa9da96ea1aa622819b
mikasd <michael.ingley@gmail.com>
2019-11-09 16:04:10 -0800

    testing

--- server/add.js
+++ server/add.js
@@ -3,20 +3,24 @@ const router = express.Router();
 const db = require('./database.js');
 
 
+//'SELECT * FROM searches WHERE tag = ? OR location = ?'
+
 router.post('/', (req, res) => {
 	console.log(req.body)
 	const insert_query = `INSERT INTO searches ( tag, location, expiration) VALUES(tag = ?, location = ?, expiration = ?)`
-	db.query('SELECT * FROM searches WHERE tag = ? OR location = ?',
-	[req.body.searchTerm, req.body.location], (err, results) => {
+	db.query('SELECT * FROM searches',
+	// [req.body.searchTerm, req.body.location],
+	 (err, results) => {
 		if(err){
 			console.log(err);
 			res.send({status: 500, results: []});
 		}
-		if(results.length > 0){
+		if(results){
 			console.log('SEARCH SUCCESSFUL')
+			console.log(results)
 			res.send({status: 200, results: results});
 		} else {
-			console.log(results)
+			console.log('this is not good ===>', results)
 			// db.query(insert_query, [req.body.searchTerm, req.body.location, req.body.date, ], (err, results) => {
 			// 	if(err){
 			// 		res.send(err);
