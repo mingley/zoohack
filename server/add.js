bbcecc4dcde7e2aa5adf65b3f0dedf2c0513a34e
mikasd <michael.ingley@gmail.com>
2019-11-09 16:27:17 -0800

    testing

--- server/add.js
+++ server/add.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {
 	console.log(req.body)
 	const insert_query = `INSERT INTO searches ( tag, location, expiration) VALUES(tag = ?, location = ?, expiration = ?)`
 	
-	db.query('SELECT * FROM searches',
+	db.query('SELECT * FROM searches WHERE (tag = ? OR location = ?)',
 	[req.body.searchTerm, req.body.location],
 	 (err, results) => {
 		if(err){
