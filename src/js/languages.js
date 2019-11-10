7a36b8cf3a94f4920b74cffdce6f9dcd9a101f9f
jinkim-tech <jhkim8268@gmail.com>
2019-11-10 08:32:20 -0800

    added Korean in language.js

--- src/js/languages.js
+++ src/js/languages.js
@@ -36,7 +36,27 @@ const spanish = {
 	}
 }
 
+const korean = {
+	index: {
+		language: "언어"
+	},
+	login: {
+		greeting: "환영합니다",
+		email: "이메일",
+		password: "비밀번호",
+		login: "로그인"
+	},
+	dashboard: {
+		location: "도시/위치",
+		searchTerm: "검색어",
+		expiration: "활동 기간",
+		submit: "제출",
+		notification: "알림: 유사한 액티비티가 이미 진행중입니다. 더 많은 정보를 원하시면 {email}을 통해 {name}에게 연락을 주십시오."
+	}
+}
+
 export default {
 	English: english,
-	Spanish: spanish
+	Spanish: spanish,
+	Korean: korean
 }
