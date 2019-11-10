bea36189f18b146024b37b10aa549a52daea3d20
mikasd <michael.ingley@gmail.com>
2019-11-09 21:34:39 -0800

    removes timeout

--- server/database.js
+++ server/database.js
@@ -10,7 +10,8 @@ const pool  = mysql.createPool({
 const cleanse_query = `DELETE FROM searches WHERE expiration < ?`
 
 function cleanse(){
-	pool.query(cleanse_query, [Math.floor(Date.now()/1000)],(err, results) => {
+	Math.floor(Date.now()/1000)
+	pool.query(cleanse_query, [Math.floor(Date.now()/1000)], (err, results) => {
 	if (err) {
 		console.error(err);
 	}
