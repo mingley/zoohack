7cee8cbd7cbecbf684334bd24656e277644371e9
jinkim-tech <jhkim8268@gmail.com>
2019-11-09 17:29:21 -0800

    changed react structure, updated margin-top

--- src/js/App.js
+++ src/js/App.js
@@ -6,30 +6,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: ''
+
     }
-    this.updateID = this.updateID.bind(this);
-    this.updatePW = this.updatePW.bind(this);
-    this.validateForm = this.validateForm.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  updateID(event) {
-    this.setState({ username: event.target.value });
-  }
-
-  updatePW(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  validateForm() {
-    return this.state.username.length > 0 && this.state.password.length > 0;
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = { username: this.state.username, password: this.state.password }
+  handleSubmit(username, password) {
+    const data = { username: username, password: password }
 
     fetch('/login', {
       method: 'POST',
@@ -44,7 +27,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Login updateID={this.updateID} updatePW={this.updatePW} validateForm={this.validateForm} handleSubmit={this.handleSubmit} />
+        <Login handleSubmit={this.handleSubmit} />
       </div>
     );
   }
--- src/js/components/login/Login.js
+++ src/js/components/login/Login.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
 class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+		}
+		this.validateForm = this.validateForm.bind(this);
+		this.updateID = this.updateID.bind(this);
+		this.updatePW = this.updatePW.bind(this);
+	}
+
+	validateForm() {
+		return this.state.username.length > 0 && this.state.password.length > 0;
+	}
+
+	updateID(event) {
+		this.setState({ username: event.target.value });
+	}
+
+	updatePW(event) {
+		this.setState({ password: event.target.value });
+	}
 
 	render() {
 		return (
 			<form className='center' onSubmit={this.props.handleSubmit}>
-				<h5>Login</h5>
+				<h5>Welcome</h5>
 				<hr></hr>
 				<div>
-					<input onChange={this.props.userInput} type='text' placeholder='id' id='username' onChange={this.props.updateID} />
+					<input onChange={this.updateID} type='text' placeholder='id' id='username' />
 				</div>
 				<div>
-					<input type='password' placeholder='password' id='password' onChange={this.props.updatePW} />
+					<input onChange={this.updatePW} type='password' placeholder='password' id='password' />
 				</div>
 				<div>
-					<button type='submit' disabled={!this.props.validateForm()}>Login</button>
+					<button onClick={() => this.props.handleSubmit(this.state.username, this.state.password)} type='submit' disabled={!this.validateForm()}>Login</button>
 				</div>
 			</form>
 		);
